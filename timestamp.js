6195f67570e1cd0842d4dc7070ee1c0fc74d90f7
ds902022 <33606227+ds902022@users.noreply.github.com>
2022-02-21 22:31:57 +0000

    restructure parsing, further validate input

--- timestamp.js
+++ timestamp.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 
+var target = null;
+
 const embedMessage = new Discord.MessageEmbed()
     .setColor("#00FFFF")
     .setTitle("Local time:")
@@ -7,67 +9,53 @@ const embedMessage = new Discord.MessageEmbed()
 
 module.exports = {
     generateTimestamp: (targetChannel, words) => {
+        target = targetChannel;
         if (words[1] === undefined) {
             targetChannel.send(
-                "Not following valid formats: \nhh:mm\ndd/mm hh:mm\ndd/mm/yyyy hh:mm"
+                "Valid inputs: \n!time hh:mm\n!time hh:mm dd/mm\n!time hh:mm dd/mm/yyyy"
+            );
+            return;
+        }
+        if (words[1] === "-help" || words[1] === "help") {
+            targetChannel.send(
+                "Valid inputs: \n!time hh:mm\n!time hh:mm dd/mm\n!time hh:mm dd/mm/yyyy"
             );
             return;
         }
         date = new Date();
         if (words[1].indexOf(":") !== -1) {
+            if (words[2] !== undefined && words[2].indexOf("/") !== -1) {
+                days = words[2].split("/");
+                if (days[2] !== undefined) {
+                    year = makeInt(days[2]);
+                    if (year >= 1970) {
+                        date.setYear(year);
+                    } else {
+                        error(targetChannel);
+                        return;
+                    }
+                }
+                day = makeInt(days[0]);
+                month = makeInt(days[1]);
+                if (day > 0 && day < 32 && month > 0 && month < 13) {
+                    date.setDate(day);
+                    date.setMonth(month - 1);
+                }
+            }
             times = words[1].split(":");
-            hour = parseInt(times[0]);
-            minute = parseInt(times[1]);
-            if (isNaN(hour) || isNaN(minute)) {
-                return;
-            }
+            hour = makeInt(times[0]);
+            minute = makeInt(times[1]);
             if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
                 date.setHours(hour);
                 date.setMinutes(minute);
-            }
-        } else if (words[1].indexOf("/") !== -1) {
-            if (words[2] === undefined) {
+            } else {
+                error(targetChannel);
                 return;
-            }
-
-            days = words[1].split("/");
-            if (days[2] !== undefined) {
-                year = parseInt(days[2]);
-                if (isNaN(year)) {
-                    return;
-                }
-                if (year >= 1970) {
-                    date.setYear(year);
-                }
-            }
-
-            day = parseInt(days[0]);
-            month = parseInt(days[1]);
-            if (isNaN(day) || isNaN(month)) {
-                return;
-            }
-            if (day > 0 && day < 32 && month > 0 && month < 13) {
-                date.setDate(day);
-                date.setMonth(month - 1);
-            }
-
-            times = words[2].split(":");
-            hour = parseInt(times[0]);
-            minute = parseInt(times[1]);
-            if (isNaN(hour) || isNaN(minute)) {
-                return;
-            }
-            if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
-                date.setHours(hour);
-                date.setMinutes(minute);
             }
         } else {
-            targetChannel.send(
-                "Not following valid formats: \nhh:mm\ndd/mm hh:mm\ndd/mm/yyyy hh:mm"
-            );
+            error(targetChannel);
             return;
         }
-
         unixTime = parseInt(date.getTime() / 1000);
         embedMessage.setDescription(`<t:${unixTime}:F>`);
         embedMessage.fields = [];
@@ -78,3 +66,20 @@ module.exports = {
         targetChannel.send(embedMessage);
     },
 };
+
+function error(targetChannel) {
+    targetChannel.send(
+        "Not following valid formats: \nhh:mm\nhh:mm dd/mm\nhh:mm dd/mm/yyyy"
+    );
+}
+
+function makeInt(val) {
+    out = parseInt(val);
+    if (isNaN(out)) {
+        out = -1;
+    }
+    if (out.toString() !== val && "0" + out.toString() !== val) {
+        out = -1;
+    }
+    return out;
+}
